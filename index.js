66e44216834141a7a54b85e24720a10b9fcc39d7
Luxy <luxydmr020904@163.com>
2025-07-29 18:36:47 +0800

    Remove Unnecessary lib

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const express = require('express')
 const request = require('request')
-const crypto = require('crypto')
+// const crypto = require('crypto')
 
 const app = express()
 app.use(express.json())
 
 // 创建32位随机字符串
-function generateRandom32String() {
-  // 创建 16 字节（128位）的 Uint8Array，最终转换为 32 位十六进制字符串
-  const array = new Uint8Array(16);
-  crypto.getRandomValues(array); // 填充随机值
-
-  // 将字节数组转换为十六进制字符串
-  return Array.from(array, byte =>
-      byte.toString(16).padStart(2, '0')
-  ).join('');
-}
+// function generateRandom32String() {
+//   // 创建 16 字节（128位）的 Uint8Array，最终转换为 32 位十六进制字符串
+//   const array = new Uint8Array(16);
+//   crypto.getRandomValues(array); // 填充随机值
+//
+//   // 将字节数组转换为十六进制字符串
+//   return Array.from(array, byte =>
+//       byte.toString(16).padStart(2, '0')
+//   ).join('');
+// }
 
 const mchId = process.env.mch_id; // 商户号
 const appId = process.env.app_id; // 小程序appID
@@ -43,36 +43,6 @@ app.post('/unifiedOrder', async function (req, res) {
     }
   }
 
-  const payinfo = {
-    appid : appId, // 小程序id
-    mchid : mchId,
-    description : text,
-    out_trade_no : generateOrderId(), // 商户系统内部订单号
-    attach : "自定义数据说明",
-    notify_url : "https://www.weixin.qq.com/wxpay/pay.php", // 商户接收支付成功回调通知的地址
-    "goods_tag" : "WXG", // 订单优惠标记
-    amount : {
-      total : totalPrice,
-      currency : "CNY"
-    },
-    payer : {
-      openid : openid
-    },
-    "detail" : {
-      "cost_price" : 608800,
-      "invoice_id" : "微信123",
-      "goods_detail" : [
-        {
-          "merchant_goods_id" : "1246464644",
-          "wechatpay_goods_id" : "1001",
-          "goods_name" : "iPhoneX 256G",
-          "quantity" : 1,
-          "unit_price" : 528800
-        }
-      ]
-    }
-  }
-
   console.log('[unifiedOrder]请求体', payreq)
   const info = await callpay('unifiedorder', payreq)
   console.log('[unifiedOrder]响应体', info)
@@ -85,9 +55,6 @@ app.listen(80, function () {
 
 function callpay (action, paybody) {
   const method = 'POST' // HTTP对应的方法
-
-  const timestamp = `${Date.now()}` // 当前时间戳
-  const randomStr = generateRandom32String(); // 随机32位字符串
 
   // 将paybody转换成一行
   const body = JSON.stringify(paybody)
